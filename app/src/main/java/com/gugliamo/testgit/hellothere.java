3bf2355ea60c9f69dd50e411029a65f52df3788e
n01318477 <n01318477@humbermail.ca>
2020-09-25 15:49:28 -0400

    added another activity

---
+++ app/src/main/java/com/gugliamo/testgit/hellothere.java
@@ -0,0 +1,15 @@
+package com.gugliamo.testgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hellothere extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hellothere);
+        //some changes
+    }
+}
